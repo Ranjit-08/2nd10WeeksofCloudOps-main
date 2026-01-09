c0f0b7a23f29e2b85d104213c1430c931d8c7b79
RJ-08 <ranjitsamalarj@gmail.com>
2026-01-09 13:28:24 +0530

    Change API_BASE_URL to new AWS ELB endpoint
    Updated API_BASE_URL to point to the new AWS ELB endpoint.

--- client/src/pages/config.js
+++ client/src/pages/config.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = "http://54.226.157.73:84";  // For reverse proxy it is mandatory so dont change
+const API_BASE_URL = "http://a9634bf552d904a6bbd3380628b898b6-1402813653.us-east-1.elb.amazonaws.com";  // For reverse proxy it is mandatory so dont change
 
 // const API_BASE_URL = "http://98.92.112.243:84"
 //const API_BASE_URL = "http://backend.515800584282.realhandsonlabs.net";
